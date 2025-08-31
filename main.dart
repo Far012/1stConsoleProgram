7074954b07334ab468c9ce5b77257f824c8f5576
Farhad <farhad.ahmadi@outlook.de>
2025-08-31 22:20:57 +0200

    feinschliff

--- main.dart
+++ main.dart
@@ -1,116 +1,158 @@
-import 'dart:io';                                       //Import der dart:io Bibliothek für Ein- und Ausgabe
+import 'dart:io'; // Import der dart:io Bibliothek für die Ein- und Ausgabe
 
-void main() {
-  print("Willkommen zum Dart-Quiz!");                             //Begrüßung
-  print("Beantworte die folgenden Fragen mit a, b, c oder d.");   //Anleitung
+void main() {                                         // Einstiegspunkt meiner Anwendung
+  print("Willkommen zum Dart-Quiz!");                 // Begrüßung
+  print("Beantworte die Fragen mit A, B, C oder D."); // Anleitung
 
-  // Name des Spielers
-  print("Bitte gib deinen Namen ein:");                 //Eingabe des Namens
-  String? name = stdin.readLineSync();                  //Einlesen des Namens
-
-  print("");                                            //Leerzeile
-  print("Hallo, $name! Viel Erfolg!");                  //Begrüßung mit Namen
+  print("Bitte gib deinen Namen ein:");
+  String? name = stdin.readLineSync();                // Einlesen des Spielernamens
+  if (name == null || name.isEmpty) {                 // Null-Prüfung und Fallback-Logik
+    name = "Spieler";                                 // Standardname wenn kein Name eingegeben wurde
+  }
+  print("");
+  print("Hallo, $name! Viel Erfolg beim Quiz!");      // Begrüßung mit Namen
   print("");
 
-  int punkte = 0;                                       //Punkte-Zähler 
+  int punkte = 0;                                     // Initialisierung des Punkte-Zählers
 
   // Frage 1
-  print("Frage 1: Was ist Dart?"); 
-  print("a) Eine Programmiersprache");
-  print("b) Ein Betriebssystem");
-  print("c) Ein Webbrowser");
-  print("d) Ein Texteditor");
-  String? antwort1 = stdin.readLineSync();              //Einlesen der Antwort
-  antwort1 = antwort1?.trim().toLowerCase();            //Antwort bereinigen auf Leerzeichen(trim) und in Kleinbuchstaben(toLowerCase) umwandeln
+  print("Frage 1: Was ist Dart?");
+  print("--------------------------");
+  print("A) Eine Programmiersprache");
+  print("B) Ein Betriebssystem");
+  print("C) Ein Webbrowser");
+  print("D) Ein Texteditor");
+  String? antwort1 = stdin.readLineSync()?.trim().toLowerCase(); // (trim) entfernt Leerzeiche, (toLowerCase) sorgt für Kleinbuchstaben = verhindert Eingabefehler
 
-  if (antwort1 == 'a') {                                //Überprüfung der Antwort 
-    punkte++;                                           //Punkte erhöhen
-    print("Richtig!");                                  //Richtige Antwort 
-    print("");
-  } else {
-    print("Falsch! Die richtige Antwort ist a) Eine Programmiersprache.");  //Falsche Antwort
-    print("");
+  switch (antwort1) {                                 // Verwendung von switch-case für die Antwortüberprüfung, das macht den Code übersichtlicher
+    case 'a':
+      punkte++;                                       // Jede richtige Antwort erhöht die Punktzahl in Variable punkte um 1
+      print("Richtig!");
+      print("");
+    default:
+      print("Falsch! Die richtige Antwort ist A) Eine Programmiersprache.");
+      print("");
   }
 
   // Frage 2
   print("Frage 2: Wer entwickelt Dart?");
-  print("a) Microsoft");
-  print("b) Google");
-  print("c) Apple");
-  print("d) Facebook");
-  String? antwort2 = stdin.readLineSync();
-  antwort2 = antwort2?.trim().toLowerCase();
+  print("--------------------------");
+  print("A) Microsoft");
+  print("B) Google");
+  print("C) Apple");
+  print("D) Facebook");
+  String? antwort2 = stdin.readLineSync()?.trim().toLowerCase();
 
-  if (antwort2 == 'b') {
-    punkte++;
-    print("Richtig!");
-    print("");
-  } else {
-    print("Falsch! Die richtige Antwort ist b) Google.");
-    print(""); 
+  switch (antwort2) {
+    case 'b':
+      punkte++;
+      print("Richtig!");
+      print("");
+    default:
+      print("Falsch! Die richtige Antwort ist B) Google.");
+      print("");
   }
 
   // Frage 3
   print("Frage 3: Wofür wird Dart hauptsächlich verwendet?");
-  print("a) Mobile App-Entwicklung");
-  print("b) Webentwicklung");
-  print("c) Serverseitige Entwicklung");
-  print("d) Alle oben genannten");
-  String? antwort3 = stdin.readLineSync();
-  antwort3 = antwort3?.trim().toLowerCase();
+  print("--------------------------");
+  print("A) Mobile App-Entwicklung");
+  print("B) Webentwicklung");
+  print("C) Serverseitige Entwicklung");
+  print("D) Alle oben genannten");
+  String? antwort3 = stdin.readLineSync()?.trim().toLowerCase();
 
-  if (antwort3 == 'd') {
-    punkte++;
-    print("Richtig!");
-    print("");
-  } else {
-    print("Falsch! Die richtige Antwort ist d) Alle oben genannten.");
-    print("");
+  switch (antwort3) {
+    case 'd':
+      punkte++;
+      print("Richtig!");
+      print("");
+    default:
+      print("Falsch! Die richtige Antwort ist D) Alle oben genannten.");
+      print("");
   }
 
   // Frage 4
   print("Frage 4: Welches Framework wird häufig mit Dart verwendet?");
-  print("a) React");
-  print("b) Angular");
-  print("c) Flutter");
-  print("d) Vue.js");
-  String? antwort4 = stdin.readLineSync();
-  antwort4 = antwort4?.trim().toLowerCase();
+  print("--------------------------");
+  print("A) React");
+  print("B) Angular");
+  print("C) Flutter");
+  print("D) Vue.js");
+  String? antwort4 = stdin.readLineSync()?.trim().toLowerCase();
 
-  if (antwort4 == 'c') {
-    punkte++;
-    print("Richtig!");
-    print("");
-  } else {
-    print("Falsch! Die richtige Antwort ist c) Flutter."); 
-    print("");
+  switch (antwort4) {
+    case 'c':
+      punkte++;
+      print("Richtig!");
+      print("");
+    default:
+      print("Falsch! Die richtige Antwort ist C) Flutter.");
+      print("");
   }
-  
+
   // Frage 5
   print("Frage 5: Welche Datei-Endung haben Dart-Dateien?");
-  print("a) .dart");
-  print("b) .js");
-  print("c) .java");
-  print("d) .py");
-  String? antwort5 = stdin.readLineSync();
-  antwort5 = antwort5?.trim().toLowerCase();
+  print("--------------------------");
+  print("A) .dart");
+  print("B) .js");
+  print("C) .java");
+  print("D) .py");
+  String? antwort5 = stdin.readLineSync()?.trim().toLowerCase();
 
-  if (antwort5 == 'a') {
-    punkte++;
-    print("Richtig!");
-    print("");
-  } else {
-    print("Falsch! Die richtige Antwort ist a) .dart.");
-    print("");
+  switch (antwort5) {
+    case 'a':
+      punkte++;
+      print("Richtig!");
+      print("");
+    default:
+      print("Falsch! Die richtige Antwort ist A) .dart.");
+      print("");
   }
 
   // Ergebnis
   print("Quiz beendet! Du hast $punkte von 5 Punkten erreicht.");
   if (punkte == 5) {
-    print("Hervorragend! Du bist ein Dart-Experte!");
+    print("Hervorragend, $name! Du bist ein Dart-Experte!");
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠞⠛⠉⢙⡛⣶⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⠋⠀⠀⠀⢰⠁⠀⠀⠉⢻⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠇⠀⠀⠀⠀⠘⠀⠀⠀⠀⠀⠹⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠜⢹⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⡶⠶⠮⠭⠵⢖⠒⠿⢤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+    print("⠀⠀⠀⠀⠀⠀⠀⣠⡶⠿⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠲⣄⡀⠀⠀⠀⠀⠀⣰⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+    print("⠀⠀⠀⠀⢀⣴⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠚⠿⡷⣄⣀⣴⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+    print("⠀⠀⠀⣠⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠪⣻⣄⠀⠀⠀⣀⣀⠤⠴⠒⠚⢋⣭⣟⣯⣍⠉⠓⠒⠦⠤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+    print("⠀⠀⣼⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⡝⣧⠔⠋⠁⠀⣀⠤⠔⣶⣿⡿⠿⠿⠿⠍⠉⠒⠢⢤⣀⠈⠑⠦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+    print("⠀⣼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡦⠤⢤⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣞⣧⠤⠒⠉⠀⠀⠾⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢲⣴⣾⣷⢤⡀⠀⠀⠀⠀⠀⠀⠀");
+    print("⢰⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡜⠀⠀⠀⠀⠈⠙⠳⢦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⠟⢿⣿⣧⠙⢦⠀⠀⠀⠀⠀⠀");
+    print("⡜⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠿⣦⣄⡀⠀⠐⢄⠀⠀⠀⢻⡇⠀⠀⠀⠀⡠⢊⣭⣬⣭⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⡀⠀⠑⣄⠀⠀⠀⠀");
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣦⡀⠀⠑⢄⣀⢿⠇⠀⠀⠀⡜⣼⠟⠁⠀⠀⠉⢿⡄⠀⠀⠀⠀⣠⠤⠤⠤⣀⡀⠈⠙⡄⠀⠈⢆⠀⠀⠀");
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣝⢦⡀⠀⣠⡞⠢⢄⠀⡜⣼⠁⣠⣴⣶⢦⡀⠀⢻⠀⠀⢀⣎⡴⠟⠛⠛⠶⣝⢦⠀⠘⡄⠀⠈⢧⠀⠀");
+    print("⢢⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢦⢻⡾⠋⠀⣀⣀⠁⠁⡇⢰⢿⣄⣿⣎⢷⠀⢸⡇⠀⢸⡝⢀⣤⣄⡀⠀⠙⢷⡀⠀⢱⠀⠀⠈⡇⠀");
+    print("⠸⡆⠀⠀⠀⠀⠀⠀⠀⠀⡸⠀⠀⠈⠙⣳⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠟⣧⠀⢀⣀⣀⡉⠱⣿⣼⣆⢿⠻⣯⡞⠀⢸⡇⠀⢸⣷⣏⢙⣿⡻⡆⠀⠀⢳⠀⠀⠀⠀⠀⢸⡀");
+    print("⠀⢳⡀⠀⠀⠀⠀⠀⠉⠚⠁⠀⠀⠀⠀⠀⠉⠻⢿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡏⠈⠁⠀⠀⠉⠢⠈⠛⢻⣿⠿⠛⠁⢀⣿⠇⠀⠈⣿⣿⢿⡟⣧⡷⠀⠀⢸⡄⠀⠀⠀⠀⠈⡇");
+    print("⠀⠈⢷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⢧⡀⠀⠀⠀⠢⠤⠔⡽⠁⠚⠉⠉⠉⢗⢷⣄⡠⣀⢻⣆⣀⣠⡿⠋⠀⠀⠀⠈⢿⡷⠿⠟⠁⠀⠀⣼⠀⠀⠀⠀⠀⠀⢱");
+    print("⠀⠀⠀⠻⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢣⠻⡄⠀⢀⣀⡤⠞⠁⠀⠀⠀⠀⠀⠘⢦⡈⠁⠀⠀⠸⡟⠉⠀⠀⠀⠀⠀⠀⠀⠙⢦⣀⣤⡶⠟⠉⠙⠒⠀⠀⠀⠀⢘");
+    print("⠀⠀⠀⠀⠈⠳⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⡀⠀⡇⣿⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣤⡀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⢠⠞⠍⠠⠤⠒⠂⢄⠀⠀⠀⠀⠀⢸");
+    print("⠀⠀⠀⠀⠀⠀⠈⠙⠲⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢀⣽⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⡉⠓⠦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⣉⡿⠓⠲⠄⠀⠀⠀⠀⡆");
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠶⢤⣤⣄⣀⣠⣤⡤⠶⠛⣿⡀⠀⢢⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣄⠀⠀⠉⠙⠒⠲⠤⠤⠤⣤⣤⡤⠖⠚⠁⠀⠀⠀⠀⠀⠀⠀⢰⠃");
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣷⠀⠀⠱⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣦⣄⡀⠀⠀⠀⢀⣼⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡞⠀");
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣧⡀⠀⠙⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢫⠙⢻⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⠁⠀");
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢳⡱⡀⠀⠀⠣⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠳⣔⢹⡉⢻⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡞⠁⠀⠀");
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣆⠀⠀⠈⠢⡀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠳⣥⣠⣹⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⠀⠀⠀⠀");
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⣕⢄⠀⠀⠈⠂⠀⠀⠀⠀⠀⠀⠀⠈⠓⠢⠌⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠃⠀⠀⠀⠀⠀");
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⢷⡦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠔⠋⠀⠀⠀⠀⠀⠀⠀");
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠺⢕⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠴⠚⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠻⠤⢄⣀⣀⣀⣀⣀⣀⣠⠤⠴⠒⠊⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+
   } else if (punkte >= 3) {
-    print("Gut gemacht! Du kennst dich gut mit Dart aus.");
+    print("Gut gemacht, $name! Du kennst dich gut mit Dart aus.");
+
   } else {
-    print("Du solltest mehr über Dart lernen.");
+    print("Oh oh, $name! Du kannst noch einiges über Dart lernen.");
   }
+
+  print("Danke fürs Mitspielen!");
 }
